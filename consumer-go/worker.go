ba90327e47e0834a9e060a41891c46aab33bbc82
Ranyel Cerracena <rcerracena@gmail.com>
2025-12-03 08:44:20 -0300

    Conclusão do producer e inicialização do worker.go

---
+++ consumer-go/worker.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/rabbitmq/amqp091-go"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const (
+	RABBITMQ_URL   = "amqp://guest:guest@rabbitmq:5672/"
+	QUEUE_NAME     = "weather_data_queue"
+	NESTJS_API_URL = "http://nestjs-api:3000/api/weather/logs"
+)
+
+type WeatherLog struct {
+	Location     string  `json:"location"`
+	Temperature  float64 `json:"temperature"`
+	WindVelocity float64 `json:"wind_velocity"`
+	Timestamp    string  `json:"timestamp"`
+}
+
+func processAndSendToAPI(messageBody []byte) {
+	var logData WeatherLog
+
+	if err := json.Unmarshal(messageBody, &logData); err != nil {
+		log.Printf(" Erro ao desserializar JSON: %v", err)
+		return
+	}
+
+	jsonBody, err := json.Marshal(logData)
+	if err != nil {
+		log.Printf("Erro ao Serializar dados para NestJS: %v", err)
+		return
+	}
+
+	client := http.Client{Timeout: 5 * time.Second}
+	
+	resp, err := client.Post(
+		NESTJS_API_URL,
+		"application/json",
+		bytes.NewBuffer(jsonBody),	
+	)
+
+	if err != nil {
+		log.Printf("Erro de rede/timeout ao enviar para NestJS: %v", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusCreated && resp.StatusCode != http.StatusOK { 
+		
+	}
+
+	log.Printf("Simulando envio dos dados para NestJS: %s", string(messageBody))
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %v", msg, err)
+	}
+}
+
+func main() {
+	log.Println("Go Worker Iniciado!")
+
+	conn, err := amqp.Dial(RABBITMQ_URL)
+	failOnError(err, "Falha ao conectar ao RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Falha ao abrir um Canal")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		QUEUE_NAME,
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "Falha ao declarar uma fila")
+
+	log.Printf("Esperando por mensagens na fila '%s'. Para sair, pressione CTRL+C", q.name)
+
+	msgs, err := ch.Consume(
+		q.Name,
+		"",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "Falha ao registrar um consumidor")
+
+	var forever chan struct{}
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Mensagem Recebida: %s", d.body)
+
+			processAndSendToAPI(d.body)
+
+			d.Ack(false)
+		}
+
+	}()
+	<-forever
+}
+
--- producer-python/producer.py
+++ producer-python/producer.py
@@ -1,24 +1,86 @@
-import requests
+import requests, pika, time
 import json
 
-API_URL = "https//api.open-meteo.com/v1/forecast"
-
+API_URL = "https://api.open-meteo.com/v1/forecast"
+RABBITMQ_HOST = "rabbitmq"
+QUEUE_NAME = "weather_data_queue"
 PARAMS = {
     "latitude": -15.55,
     "longitude": -47.34,
     "current_weather": "true",
-    "timezone": "America/Sao_Paulo"
+    "timezone": "America/Sao_Paulo",
 }
+INTERVALO_SEGUNDOS = 60  
 
-try:
-    response = requests.get(API_URL, params=PARAMS)
-    response.raise_for_status()
-    data = response.json()
 
-    temperature = data['current_weather']['temperature']
-    wild_velocity = data['current_weather']['windspeed']
+def main_loop():
+    while True:
+        print("Iniciando nova iteração de coleta e envio de dados...")
+        
+        queue_data = fetch_weather_data()
+        
+        if queue_data:
+            send_to_queue(queue_data)
+        else:
+            print("Coleta de dados falhou. Pulando envio para a fila.")
+        
+        print(f"Ciclo finalizado. Dormindo por {INTERVALO_SEGUNDOS} segundos...")
+        time.sleep(INTERVALO_SEGUNDOS)
 
-    print(f"Temperatura Atual: {temperature}ºC")
-    print(f"Velocidade do Vento: {wild_velocity} km/h")
 
+def send_to_queue(data_dict):
+    try:
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host=RABBITMQ_HOST)
+        )
+        channel = connection.channel()
+        channel.queue_declare(queue=QUEUE_NAME, durable=True)
+
+        message = json.dumps(data_dict)
+        channel.basic_publish(
+            exchange="",
+            routing_key=QUEUE_NAME,
+            body=message,
+            properties=pika.BasicProperties(
+                delivery_mode=2,
+            ),
+        )
+        print(f"Enviado {message} para a fila {QUEUE_NAME} com sucesso.")
+
+        connection.close()
+    except pika.exceptions.AMQPConnectionError as e:
+        print(f"Erro ao conectar com o RabbitMQ: {e}")
+    except Exception as e:
+        print(f"Erro ao enviar mensagem para a fila: {e}")
+
+
+def fetch_weather_data():
+    try:
+        response = requests.get(API_URL, params=PARAMS)
+        response.raise_for_status()
+        data = response.json()
+
+        temperature = data["current_weather"]["temperature"]
+        wind_velocity = data["current_weather"]["windspeed"]
+
+        print(f"Temperatura Atual: {temperature}ºC")
+        print(f"Velocidade do Vento: {wind_velocity} km/h")
+
+        queue_data = {
+            "location": "Formosa, GO",
+            "temperature": temperature,
+            "wind_velocity": wind_velocity,
+            "timestamp": data["current_weather"]["time"],
+        }
+        
+        return queue_data
+
+    except requests.exceptions.RequestException as e:
+        print(f"Erro ao conectar com a api: {e}")
+        return None
+    except KeyError as e:
+        print(f"Erro ao extrair dados da resposta: Campo {e} não encontrado.")
+        return None
     
+if __name__ == "__main__":
+    main_loop()
